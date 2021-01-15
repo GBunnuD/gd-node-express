695f50ff3f266713053c0268b444660ef996175c
GD-Prasad <g.d.prasad004@gmail.com>
2021-01-15 01:26:06 -0600

    Added help, contact and about.

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
     res.send("Hello World this is GD's App!")
 })
 
+app.get('/about', (req, res) => {
+    res.send("You have requested the about page of GD's App!")
+})
+app.get('/contact', (req, res) => {
+    res.send("This is the Contact Page of GD's App!")
+})
+app.get('/help', (req, res) => {
+    res.send("Help Page of GD's App!")
+})
+
+app.get('/help/:name', (req, res) => {
+    res.send(`help response for ${req.params.name}`)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
